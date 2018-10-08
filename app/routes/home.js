8c1049017a616a968b49377c7c81241e806919b9
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-10-08 16:07:39 -0300

    tela de login

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,9 @@
 module.exports = function(app){
     app.get('/', function(req, res){
-        res.render('index');
+        res.render('login');
+    });
+    app.post('/', function(req, res){
+        console.log('Vc está fazendo um POST');
+        res.render('login');
     });
 }
