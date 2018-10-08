78d3b55926533b723ab5e0939afaadb695f962dc
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-10-08 15:50:11 -0300

    First commit

---
+++ configs/express.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+var bodyParser = require('body-parser');
+var load = require('express-load');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+//sempre será executado ao receber uma requisição
+app.use(bodyParser.json());//body-parser aceita o tipo JSON
+
+app.set('view engine', 'ejs');
+
+//pasta onde ficarão as views
+app.set('views', './app/views');
+
+app.listen(port, function(){
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+module.exports = function(){
+    load('routes', {cwd: 'app'})//carrega os módulos relacionados as rotas
+		.then('infra')//correga os módulos relacionados ao DAO
+		.into(app);
+    return app;
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+var app = require('./configs/express')();
