ecf74dcfa2040aca233ffc188e801ee23869dca4
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-10-10 21:03:18 -0300

    configurado para acesso ao mysql do heroku

--- app/infra/connFactory.js
+++ app/infra/connFactory.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 function connFactory(){
     return conn = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: ''
+        host: 'us-cdbr-iron-east-01.cleardb.net',
+        user: 'b6cb32128b8710',
+        password: '64dab02f',
+        database: 'heroku_9c64204be4edb81'
     });
 }
 
