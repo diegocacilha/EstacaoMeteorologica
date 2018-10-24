c33919dde69c56ce204bac883226fe7dc704617b
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-10-24 19:57:06 -0300

    Cadastro de usuários

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -1,5 +1,20 @@
 module.exports = function(app){
-    app.get('/usuario', function(req, res){
+    app.get('/usuario', (req, res) => {
         res.render('usuario');
+    });
+    app.post('/usuario', (req, res) => {
+        var conn = app.infra.connFactory();
+        var usuarioDAO = new app.infra.UsuarioDAO(conn);
+        
+        usuarioDAO.create(req.body, function(err, result){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('home');
+            }
+
+        });
+
+        conn.end();
     });
 }
