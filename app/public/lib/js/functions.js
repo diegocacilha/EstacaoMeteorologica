18773245b345e5ca8df9f3b00ef2060710fa3fea
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-11-02 22:18:05 -0300

    Comentários para o DAVIZÃO

--- app/public/lib/js/functions.js
+++ app/public/lib/js/functions.js
@@ -1,3 +1,12 @@
+/**
+ * Esta function está no cabeçalho da página principal. No caso estamos fazendo isso no 
+ * documento home.ejs. Ok DAVIZÃO?
+ * 
+ * @param {String} method Método http 
+ * @param {String} url Endpoint 
+ * @param {JSON} data 
+ * @param {Function} callback Function que será exacutada quando retornar do DB 
+ */
 function sendRequest(method, url, data, callback){
     var req = new XMLHttpRequest();
     data = JSON.stringify(data);
