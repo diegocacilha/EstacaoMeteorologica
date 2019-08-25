a9b06a43d5fcc4414e142cc2c23c8db3b4833993
Diego Armando Cacilha <cacilha.tec@gmail.com>
2019-08-25 01:39:19 -0300

    deixei de usar o express-load e comecei a usar consign

--- app/routes/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        if(!req.session.uniqueId){
-            res.redirect('/login');
-            return;
-        }
-        res.render('home');
-    });
-}
