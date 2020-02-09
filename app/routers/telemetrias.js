7b1b48f2d92d1ef74a21385b61513470706228d9
Diego Armando Cacilha <cacilha.tec@gmail.com>
2020-02-09 02:13:29 -0300

    Edição de telemetrias pronta

--- app/routers/telemetrias.js
+++ app/routers/telemetrias.js
@@ -1,20 +1,20 @@
 module.exports = (app) => {
     app.get('/telemetrias', (req, res) => {
-        if(!req.session.uniqueId){
+        if (!req.session.uniqueId) {
             res.redirect('/');
             return;
         }
         var conn = app.infra.connFactory();
         var telemetrias = new app.infra.TelemetriasDAO(conn);
-        telemetrias.lista(function(err, result){
-            if(err){
+        telemetrias.lista(function (err, result) {
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 res.format({
-                    html: function(){
-                        res.render('telemetrias', {lista: result});
-                    }, 
-                    json: function(){
+                    html: function () {
+                        res.render('telemetrias', { lista: result });
+                    },
+                    json: function () {
                         res.json(result);
                     }
                 });
@@ -22,20 +22,73 @@ module.exports = (app) => {
         });
         conn.end();
     });
-    app.post('/telemetrias/cadastro', (req, res)=>{
-        if(!req.session.uniqueId){
-            res.redirect('/');
-            return;
-        }
+    app.post('/telemetrias/cadastro', (req, res) => {
+        validaSessao(req, res);
         var conn = app.infra.connFactory();
         var telemetrias = new app.infra.TelemetriasDAO(conn);
 
-        console.log(req.body);
-        telemetrias.insert(req.body, function(err, result){
-            if(err){
+        telemetrias.insert(req.body, function (err, result) {
+            if (err) {
                 console.log(err);
             }
         });
         conn.end();
     });
+
+    app.get('/telemetrias/editar/:id', (req, res) => {
+        validaSessao(req, res);
+        var id = req.params.id;
+
+        var conn = app.infra.connFactory();
+        var telemetrias = new app.infra.TelemetriasDAO(conn);
+        telemetrias.lista_unica(id, (err, result) => {
+            if (err)
+                console.log(err);
+            else {
+                res.render('editar_telemetrias', { lista: result[0] });
+            }
+
+
+        });
+        conn.end();
+    });
+
+    app.post('/telemetrias/editar/:id', (req, res) => {
+        validaSessao(req, res);
+        var telemetria = req.body;
+
+        var conn = app.infra.connFactory();
+        var telemetrias = new app.infra.TelemetriasDAO(conn);
+        telemetrias.update(telemetria, (err, result) => {
+            if (err)
+                console.log(err);
+            else {
+                res.method = 'GET';
+                res.redirect('/telemetrias');
+            }
+        });
+        conn.end();
+    });
+
+    app.delete('/telemetrias/excluir', (req, res) => {
+        validaSessao(req, res);
+        var id = req.params.id;
+        console.log('entoru');
+        var conn = app.infra.connFactory();
+        var telemetrias = new app.infra.TelemetriasDAO(conn);
+        telemetrias.delete(id, (err, result) => {
+            if (err)
+                console.log(err);
+            else
+                res.redirect('/telemetrias');
+        });
+        conn.end();
+    });
+
+    var validaSessao = (req, res) => {
+        if (!req.session.uniqueId) {
+            res.redirect('/');
+            return;
+        }
+    };
 }
