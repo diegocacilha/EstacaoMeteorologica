aa77ee3a69cb4067ca62d28e3bd26825207d5f15
Davi Ferigotti <davi.lferigotti@gmail.com>
2020-02-14 18:44:40 -0300

    criando classes da telemetria

---
+++ app/public/lib/js/controllers/TelemetriasController.js
@@ -0,0 +1,30 @@
+class TelemetriasController{
+  constructor()
+  {
+
+    let $ = document.querySelector.bind(document);
+    this._telemetria = new Telemetria();
+    this._inputData = $('#data');
+    this._inputTemperatura = $('#temperatura');
+    this._inputPressao = $('#pressao');
+
+  }
+
+  adiciona(){
+    event.preventDefault();
+    //instanciar classes da view;
+  }
+
+
+
+  criaTelemetria(){
+
+    let telemetria = new Telemetria(
+      this._inputData.value,
+      this._inputTemperatura.value,
+      this._inputPressao.value
+    );
+
+
+  }
+}
--- app/public/lib/js/models/Telemetria.js
+++ app/public/lib/js/models/Telemetria.js
@@ -1,7 +1,7 @@
-class Telemetrias {
+class Telemetria {
 
   constructor(data, temperatura, pressao){
-    this._data = new Date();
+    this._data = new Date(data.getTime());
     this._temperatura = temperatura;
     this._pressao = pressao;
   }
