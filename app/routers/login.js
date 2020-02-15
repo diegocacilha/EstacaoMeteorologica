0a2573ee125329c047e3670558d99fee9cb0e770
Diego Armando Cacilha <cacilha.tec@gmail.com>
2020-02-15 18:52:24 -0300

    Tinha um erro GRAVE no login do backend

--- app/routers/login.js
+++ app/routers/login.js
@@ -25,7 +25,7 @@ module.exports = function(app){
                     }, 
                     json: function(){
                         res.json({
-                            status: false,
+                            status: true,
                             msg: 'Login efetuado com sucesso'
                         });
                     }
