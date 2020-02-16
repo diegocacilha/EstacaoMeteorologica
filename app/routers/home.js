86b4a912ee024f1c7449e76e7d6eb5b618a5519d
Diego Armando Cacilha <cacilha.tec@gmail.com>
2020-02-16 19:20:36 -0300

    Alterado nome de pastas, rotas e outros

--- app/routers/home.js
+++ app/routers/home.js
@@ -2,7 +2,8 @@ module.exports = (app) => {
     app.get('/home', (req, res) => {
         res.json({
             status: true,
-            msg: 'Alguma coisa que deve ser enviada pela rota /home'
+            msg: 'Alguma coisa que deve ser enviada pela rota /home', 
+            davi: 'O que eu devo te enviar aqui nessa rota? Seu alemaum'
         });
     });
 }
