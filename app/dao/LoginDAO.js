96dc2c093a0e7a5ce09a811605390fbbc65abb56
Diego Armando Cacilha <cacilha.tec@gmail.com>
2020-02-22 11:53:37 -0300

    pequeno ajuste - nada de mais

--- app/dao/LoginDAO.js
+++ app/dao/LoginDAO.js
@@ -2,7 +2,8 @@ const StaticConnection = require('../dao/StaticConnection');
 module.exports = class LoginDao{
 	lista(login, callback){
 		let conn = StaticConnection.getConnection();
-		let teste = conn.query('select * from users where email = ? and pw = ? ', [login.email, login.pw], callback);
+		let query  = 'select * from users where email = ? and pw = ? ';
+		conn.query(query, [login.email, login.pw], callback);
 		conn.end();
 	}
 
